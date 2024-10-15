3d3adf3f818c1e213b941e5c3ae2dc5351b05f77
Daniel Knittl-Frank <knittl89+git@googlemail.com>
2024-10-15 23:56:18 +0200

    Implement simple tests

---
+++ test.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+. ./format.sh
+
+assert() {
+	actual=$2
+	expected=$1
+
+	if test "$actual" = "$expected"; then
+		echo "OK:  $expected"
+	else
+		echo "FAIL: $expected != $actual"
+	fi
+}
+
+assert "$(printf '\033[;31mxxx\033[;0m\n')" "$(fmt red xxx)"
+assert "$(printf '\033[;31;1mxxx\033[;0m\n')" "$(fmt red,bold xxx)"
+assert "$(printf '\033[;1;31mxxx\033[;0m\n')" "$(fmt bold,red xxx)"
+assert "$(printf '\033[;41mxxx\033[;0m\n')" "$(fmt bg,red xxx)"
+assert "$(printf '\033[;21mxxx\033[;0m\n')" "$(fmt not,bold xxx)"
